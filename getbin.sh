97aac79da69dc69dff1e82b904fdf09b207999a6
David Konsumer <konsumer@jetboystudio.com>
2015-11-14 12:21:39 -0800

    grab ffprobe, too

--- getbin.sh
+++ getbin.sh
@@ -10,23 +10,25 @@ mkdir -p ffmpeg/linux/ia32 ffmpeg/linux/x64 ffmpeg/darwin/x64 ffmpeg/win32/ia32 
 cd ffmpeg/linux
 curl 'http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz' -o ffmpeg-32.tar.xz && 
 tar xf ffmpeg-32.tar.xz && 
-mv ffmpeg-*-32bit-static/ffmpeg ia32
+mv ffmpeg-*-32bit-static/ffmpeg ffmpeg-*-32bit-static/ffprobe ia32
 curl 'http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz' -o ffmpeg-64.tar.xz && 
 tar xf ffmpeg-64.tar.xz && 
-mv ffmpeg-*-64bit-static/ffmpeg x64
+mv ffmpeg-*-64bit-static/ffmpeg ffmpeg-*-64bit-static/ffprobe x64
 rm -rf ffmpeg-*-*-static *.tar.xz
 
 cd ../darwin
-curl "http://evermeet.cx/pub/ffmpeg/ffmpeg-$VERSION.7z" -o ffmpeg.7z && 
+curl "http://evermeet.cx/ffmpeg/ffmpeg-$VERSION.7z" -o ffmpeg.7z && 
+curl "http://evermeet.cx/ffmpeg/ffprobe-$VERSION.7z" -o ffprobe.7z
 7z e ffmpeg.7z && 
-mv ffmpeg x64
+7z e ffprobe.7z && 
+mv ffmpeg ffprobe x64
 rm ffmpeg.7z
 
 cd ../win32
 curl "http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-$VERSION-win32-static.7z" -o ffmpeg.7z &&
 7z x ffmpeg.7z
-cp ffmpeg-*-win32-static/bin/ffmpeg.exe ia32
-cp ffmpeg-*-win32-static/bin/ffmpeg.exe x64
+cp ffmpeg-*-win32-static/bin/ffmpeg.exe ffmpeg-*-win32-static/bin/ffprobe.exe ia32
+cp ffmpeg-*-win32-static/bin/ffmpeg.exe ffmpeg-*-win32-static/bin/ffprobe.exe x64
 rm -rf ffmpeg.7z ffmpeg-*-win32-static
 
 cd ../..

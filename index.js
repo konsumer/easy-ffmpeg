30a5eb0431e4487746ec20dfbeb4747da48ee3cc
David Konsumer <konsumer@jetboystudio.com>
2016-02-20 12:36:18 -0800

    bump v0.0.10

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var FfmpegCommand = require('fluent-ffmpeg')
 var path = require('path')
 
+var p = path.join(__dirname, 'ffmpeg', process.platform, arch, 'ffmpeg')
+
 // only one platform  available, but works for both
 var arch = process.arch
 if (process.platform === 'win32') {
   arch = 'ia32'
+  p += '.exe'
 }
 
-var p = path.join(__dirname, 'ffmpeg', process.platform, arch, 'ffmpeg')
 FfmpegCommand.setFfmpegPath(p)
 module.exports = FfmpegCommand

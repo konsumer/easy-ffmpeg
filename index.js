62ad4da70cb9809d6d4099066c89202faa2db37b
David Konsumer <konsumer@jetboystudio.com>
2016-02-20 15:50:51 -0800

    stupid order

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var FfmpegCommand = require('fluent-ffmpeg')
 var path = require('path')
 
-var p = path.join(__dirname, 'ffmpeg', process.platform, arch, 'ffmpeg')
+var name = 'ffmpeg'
 
 // only one platform  available, but works for both
 var arch = process.arch
 if (process.platform === 'win32') {
   arch = 'ia32'
-  p += '.exe'
+  name = 'ffmpeg.exe'
 }
+
+var p = path.join(__dirname, 'ffmpeg', process.platform, arch, name)
 
 FfmpegCommand.setFfmpegPath(p)
 module.exports = FfmpegCommand

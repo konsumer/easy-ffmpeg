9b98f58bcef21c367048f9d2588949df31a86a83
David Konsumer <konsumer@jetboystudio.com>
2016-02-20 12:28:59 -0800

    simple test and better path

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var FfmpegCommand = require('fluent-ffmpeg')
+var path = require('path')
+
 // only one platform  available, but works for both
 var arch = process.arch
 if (process.platform === 'win32') {
   arch = 'ia32'
 }
-FfmpegCommand.setFfmpegPath(require('path').join(__dirname, 'ffmpeg', process.platform, arch))
+
+var p = path.join(__dirname, 'ffmpeg', process.platform, arch, 'ffmpeg')
+FfmpegCommand.setFfmpegPath(p)
 module.exports = FfmpegCommand
---
+++ test.js
@@ -0,0 +1,12 @@
+var ffmpeg = require('./');
+
+var proc = ffmpeg('/Users/konsumer/Desktop/big_buck_bunny.mp4')
+  .videoFilters('fade=in:0:30')
+  .videoFilters('pad=640:480:0:40:violet')
+  .on('end', function() {
+    console.log('done processing input stream');
+  })
+  .on('error', function(err) {
+    console.log('an error happened: ' + err.message);
+  })
+  .save('big_buck_bunny.mp4');

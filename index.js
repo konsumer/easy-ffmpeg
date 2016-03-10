befa7e65cbbdce03f67c867201f3babe0b5ada0b
jaruba <branza.alex@gmail.com>
2016-03-10 11:55:18 +0200

    Only download ffmpeg if needed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var FfmpegCommand = require('fluent-ffmpeg')
-var path = require('path')
+var FfmpegCommand = require('fluent-ffmpeg'),
+    path = require('path'),
+    fs = require('fs');
 
 // osx: x64 / other: ia32
 var arch = process.platform == 'darwin' ? 'x64' : 'ia32';
@@ -7,7 +8,19 @@ var arch = process.platform == 'darwin' ? 'x64' : 'ia32';
 // only windows has an extension
 var ext = process.platform == 'win32' ? '.exe' : '';
 
-FfmpegCommand.setFfmpegPath( path.join(__dirname, 'ffmpeg-' + process.platform, arch, 'ffmpeg' + ext) )
-FfmpegCommand.setFfprobePath( path.join(__dirname, 'ffmpeg-' + process.platform, arch, 'ffprobe' + ext) )
+try {
+
+    // this checks if the ffmpeg folder exists in our repo, if it doesn't it will return an error
+    fs.accessSync( path.join(__dirname, 'ffmpeg-' + process.platform) , fs.F_OK);
+
+    // folder exists so we need to load ffmpeg and ffprobe from our repo
+    FfmpegCommand.setFfmpegPath( path.join(__dirname, 'ffmpeg-' + process.platform, arch, 'ffmpeg' + ext) )
+    FfmpegCommand.setFfprobePath( path.join(__dirname, 'ffmpeg-' + process.platform, arch, 'ffprobe' + ext) )
+
+} catch (e) {
+    // folder does not exist, this means that ffmpeg and ffprobe
+    // wore found somewhere else during the install process
+    // fluent-ffmpeg will set the correct paths on it's own
+}
 
 module.exports = FfmpegCommand;
--- install.js
+++ install.js
@@ -10,21 +10,47 @@ var tarball = require('tarball-extract'),
 var user = 'jaruba',
     tag = 'v0.0.1',
     repoName = 'ffmpeg-binaries',
-    package = 'ffmpeg-' + process.platform + '.tgz';
+    package = 'ffmpeg-' + process.platform + '.tgz',
+    counter = 0,
+    errors = 0;
 
-var url = 'https://github.com/' + user + '/' + repoName + '/releases/download/' + tag + '/' + package;
+function downloader() {
+    // if no ffmpeg and ffprobe are found installed
+    // we will download the one for our platform
 
-request
-  .get(url)
-  .on('error', function (err) {
-    throw err
-  })
-  .pipe(fs.createWriteStream( package ))
-  .on('close', function () {
-    tarball.extractTarball(package, __dirname, function (err, result) {
-      if (err) throw err
-      fs.unlink(package, function (err) {
-        if (err) throw err
+    var url = 'https://github.com/' + user + '/' + repoName + '/releases/download/' + tag + '/' + package;
+
+    request
+      .get(url)
+      .on('error', function (err) {
+        throw err
       })
-    })
-  })
+      .pipe(fs.createWriteStream( package ))
+      .on('close', function () {
+        tarball.extractTarball(package, __dirname, function (err, result) {
+          if (err) throw err
+          fs.unlink(package, function (err) {
+            if (err) throw err
+          })
+        })
+      })
+}
+
+function checker(err, path) {
+    if (err) errors++;
+    else if (!err && !path) errors++;
+
+    if (counter) {
+        // finished checking for ffmpeg and ffprobe
+        if (errors) {
+            // could not find ffmpeg and ffprobe installed, downloading
+            downloader();
+        }
+    } else counter++;
+}
+
+// let's try to find ffmpeg and ffprobe
+var ffmpeg = require('fluent-ffmpeg')();
+
+ffmpeg._getFfmpegPath(checker);
+ffmpeg._getFfprobePath(checker);
